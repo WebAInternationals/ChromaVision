f3651bfabbecd8dbeb5096164d769db2cae9d2b5
Just-ZJ <dunlike2big@hotmail.com>
2022-02-26 17:47:38 -0500

    refractored color blind type selection to using react

---
+++ src/ColorBlindSelection.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class ColorBlindSelection extends Component {
+  state = {
+    colorBlindTypes: [
+      "Trichromacy / Normal",
+      "Blue Cone Monochromacy / Achromatomaly",
+      "Monochromacy / Achromatopsia",
+      "Green-Weak / Deuteranomaly",
+      "Green-Blind / Deuteranopia",
+      "Red-Weak / Protanomaly",
+      "Red-Blind / Protanopia",
+      "Blue-Weak / Tritanomaly",
+      "Blue-Blind / Tritanopia",
+    ],
+  };
+
+  render() {
+    return this.state.colorBlindTypes.map((type) => (
+      <div id="" class="row my-2">
+        <div class="col-8">
+          <label>{type}</label>
+        </div>
+        <div class="col-4">
+          <label class="switch">
+            <input type="checkbox" />
+            <span class="slider round"></span>
+          </label>
+        </div>
+      </div>
+    ));
+  }
+}
+
+export default ColorBlindSelection;
