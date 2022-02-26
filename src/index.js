ab7ff719499e9313480abf85bb5cb4ffdbb3ae5c
Just-ZJ <dunlike2big@hotmail.com>
2022-02-26 17:48:36 -0500

    removed unused files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ColorBlindSelection from "./ColorBlindSelection";
-import reportWebVitals from "./reportWebVitals";
 // import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -9,8 +8,3 @@ ReactDOM.render(
   <ColorBlindSelection />,
   document.getElementById("ColorBlindSelection")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
