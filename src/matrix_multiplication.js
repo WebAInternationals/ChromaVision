7a10b59c916a3117717a17799991a64813aba599
Jiatuo Zou <90865105+jiatuoz2@users.noreply.github.com>
2022-02-27 00:10:39 -0600

    Popup (#2)
    * barebone popup
    
    * matrix multiplication
    
    Co-authored-by: ZJ <54641137+Just-ZJ@users.noreply.github.com>

---
+++ src/matrix_multiplication.js
@@ -0,0 +1,20 @@
+function dot(T_matrix, color_matrix) {
+	let rslt_matrix = [];
+	for (let i = 0; i < T_matrix.length; i++) {
+		let sum = 0;
+		for (let j = 0; j < T_matrix[i].length; j++) {
+			sum += T_matrix[i][j] * color_matrix[j];
+		}
+		rslt_matrix.push(sum);
+	}
+	return rslt_matrix;
+}
+
+function MatrixStringtoArray(matrix) {
+	let array_matrix = [];
+	let rows = matrix.split(' ');
+	rows.forEach(function(row) {
+    	array_matrix.push(row.split(','));
+  	});
+	return array_matrix;
+}
